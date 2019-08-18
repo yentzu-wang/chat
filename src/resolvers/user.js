889487c3b84d5e89ef0fc7ed5c5d8b009b5b2be4
Frank <frank7300228@gmail.com>
2019-08-18 22:46:04 +0800

    Project init

---
+++ src/resolvers/user.js
@@ -0,0 +1,9 @@
+export default {
+  Query: {
+    users: (parent, arg, context, info) => {},
+    user: (parent, arg, context, info) => {}
+  },
+  Mutation: {
+    signUp: (parent, arg, context, info) => {}
+  }
+}
---
+++ src/typeDefs/user.js
@@ -0,0 +1,25 @@
+import { gql } from "apollo-server-express"
+
+export default gql`
+  extend type Query {
+    user(id: ID!): User
+    users: [User!]!
+  }
+
+  extend type Mutation {
+    signUp(
+      eamil: String!
+      username: String!
+      name: String!
+      password: String!
+    ): User
+  }
+
+  type User {
+    id: ID!
+    email: String!
+    suername: String!
+    name: String!
+    createAt: String!
+  }
+`
